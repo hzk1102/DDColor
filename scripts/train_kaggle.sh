ee7ae0fad3e7d31e25125cfde7c56e0a6f1102f6
Hoang Nguyen <h_ff8>
2026-01-04 21:02:05 +0700

    add

--- scripts/train_kaggle.sh
+++ scripts/train_kaggle.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=3721 basicsr/train.py \
-    -opt options/train/train_ViCoW_1.0_0.5_pretrain_512_kaggle.yml --auto_resume --launcher pytorch
+    -opt options/train/train_ViCoW_1.0_0.5_freeze_g_nonorm_pretrain_512_kaggle.yml --auto_resume --launcher pytorch
